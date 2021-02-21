26e18c5e57fbdababe31b515ed37d6587351b9d7
dbranquinho <delermando@gmail.com>
2021-02-21 09:46:47 -0300

    Apha Version

---
+++ README.md
@@ -0,0 +1,444 @@
+# pyWPB
+
+`pyWPB` - Python Web Page Builder
+
+HTML `Creator` using `Python`
+
+![](readme_files/img2.svg)![](readme_files/img3.svg)![](readme_files/img4.svg)
+
+
+**key features:**
+
+- Building a Header Page
+- Style to Body tag
+- Create Tables using dataframe pandas
+- Insert images from graph application such seaborn, matplotlib and any other
+- Coloring text and styles separately
+- Personalised Footer
+- Automatically creates texto to Images and Tables
+
+## Where to use
+- Nicely to be used on embedded in Flask applications. You create a dynamic page very easy
+- You can also create dynamic pages in your Django applications
+- If you have other applications working in a batch mode, you can create many dynamics pages in background
+- You don't need to know HTML5, CSS or some stuff like that, you just know Python
+
+<BR><BR>
+<hr>
+
+## Install
+
+```shell
+pip install pywpb
+```
+<hr>
+
+## Current Modules
+
+To use any Neublis module you must instantiates all of then using the follow command:
+
+### **Header**
+
+There is only one method to create a page. And you need to run it before anything else.
+
+### **Creator**
+
+Create a new Web Page with a minimal configuration. You must run this step before any other command. You can choose you lang, if ommited, the default lan will be english.
+
+`sintax`:
+```python
+from neublis import neublis as nb
+
+h = nb.header(charset='utf-8', 
+           page_size=[21.0, 29.7, 2.0],
+           margin=[0.25, 115],
+           background='transparent',
+           title='Neublis Page without Title')
+```
+The values above are default, but you can change to:
+
+**The charset**
+- `ASCII` | `ANSI` | `ISO-8859-1` | `UTF-8`
+
+**Page Size**
+- `Right`=21.0 | `Left` 29.7 | `Margin` 2.0
+
+**Margin**
+- `margin-bottom` | `line-height`
+
+**Background**
+- You can change to any wish color 
+
+**Title**
+- Title of you page. If you don't give a text, the text above will be provided
+
+`Prints such a string:`
+
+```html
+<!DOCTYPE html PUBLIC "-// W3C // DTD XHTML 1.0 Strict // EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+	<title>Neublis Page without Title</title>
+	<meta name="generator" content="neublis"/>
+	<meta name="created" content="2021/02/17T16:44:08"/>
+	<meta name="changed" content="00:00:00"/>
+	<style type="text/css">
+		@page { size: 21.0cm 29.7cm; margin: 2.0cm }
+		p { margin-bottom: 0.25cm; line-height: 115%; background: transparent }
+	</style>
+</head>
+
+```
+
+### **Body**
+
+`sintax`:
+```python
+from neublis import neublis as nb
+
+b = nb.body(margin=0, line=100, 
+         link='#000080', 
+		 vlink='#800000',
+         lang='eng')
+```
+
+The values above are default, but you can change to:
+
+**Margin**
+- Margin of body. Normally zero.
+
+**Line**
+- Line will be `100`%
+
+**Links**
+- The color of the links on the page. The first is the `link never clicked`. The other is the `visited Link`.
+
+`Prints such a string:`
+```html
+	<body lang="eng" link="#000080" vlink="#800000" dir="ltr">
+		<p style="margin-bottom: 0cm; line-height: 100%"><br/>
+		</p>
+```
+<hr>
+
+### **Body Methods**
+
+The `Body Class` has many methods that you can see below.
+
+<hr>
+
+#### **h (headings)**
+
+The `<h1>` to `<h6>` tags are used to define HTML headings.
+
+`<h1>` defines the most important heading. `<h6>` defines the least important heading.
+
+`sintax`:
+```python
+from neublis import neublis as nb
+
+b = nb.body() # keeping default values for body
+
+b.h(text='Headings have no text', 
+    size=5, background='white', 
+    align='left', 
+	color='black', 
+	shadow=False)
+```
+
+**Text**
+- The text that you want to write on the headings
+
+**Size**
+- As previously presented, this is the size ranging from one to six
+
+**Background**
+- The background `color`. Make your choice!
+
+**Align**
+- The align of the headings. Can be:
+
+	`left` | `center` | `right` | `ustify`
+
+**Color**
+- The `color` of the text to be write on headings. Make your choice!
+
+**Shadow**
+- If you want a elegant text with shadow, just change to `True`
+
+`Prints such a string:`
+```html
+		<hr style="height:1px; border-width:0; color:gray;background-color:gray">
+```
+
+<hr>
+
+#### **Text Color**
+
+If you want a text with different color, use this method to write a new text on the page. This method don't insert line feed.
+
+`Sintax:`
+```python
+from neublis import neublis as nb
+
+b = nb.body() # keeping default values for body
+
+b.color_text(text='no text provided',
+			 color='black')
+```
+`Prints such a string:`
+```html
+<span style="color:black">this is my text</span>
+```
+
+#### **Write a Text**
+
+This method writes a text, any one that you provided in text argument.
+
+`Sintax`
+```python
+from neublis import neublis as nb
+
+b = nb.body() # keeping default values for body
+
+b.w_text('another text to see line feed',3)
+```
+There is no limit to text. If you have a big text, the better way is to load text using `wpIO`. See `loading text from file`.
+
+*line_feed* is the number of lines will be jump using tag `<BR>`.
+
+`Prints such a string:`
+```html
+another text to see line feed<BR><BR><BR>
+```
+<hr>
+
+`Sintax`
+```python
+from neublis import neublis as nb
+
+b = nb.body() # keeping default values for bod
+
+text = b.change_text_color(color='blue', text='testing text one changing color', text_to='one')
+```
+
+`Prints such a string:`
+```html
+testing text <span style="color:blue">one </span> changing color<BR><BR>
+```
+After you changed the color text, you must write the text using `w_text` method.
+
+<hr>
+
+#### **Bold Text**
+
+Made bold text.
+
+`Sintax`
+```python
+from neublis import neublis as nb
+
+b = nb.body() # keeping default values for bod
+
+text = b.bold_text(self, text='no text provided', text_to='text')
+```
+
+The word `text` will be bold.
+
+<hr>
+
+#### **Italic Text**
+
+Made Italic text.
+
+`Sintax`
+```python
+from neublis import neublis as nb
+
+b = nb.body() # keeping default values for bod
+
+text = b.italic_text(self, text='no text provided', text_to='text')
+```
+<hr>
+
+#### **Horizontal Line**
+
+The `<hr>` tag defines a thematic break in an HTML page. 
+The `<hr>` element is most often displayed as a horizontal rule that is used to separate content (or define a change) in an HTML page.
+
+`Sintax`
+```python
+from neublis import neublis as nb
+
+b = nb.body() # keeping default values for bod
+
+b.hline(height=1, border=0, color='gray', background='gray')
+```
+
+#### **Writing Table**
+
+`Sintax`
+```python
+from neublis import neublis as nb
+
+b = nb.body() # keeping default values for bod
+
+df = pd.DataFrame({ 'id': [1,2,3], 
+					'Elapsed_time': [11,21,31],
+					'Total_Value': [3.5, 4.2 , 5.1]})
+
+b.w_table(df,alt_text='My Table without footer',foot=False)
+```
+**data**
+- The `data` is a `Pandas DataFrame`.
+- Big dataset with many columns or lines is not a good idea.
+- May be use `head()` to is good way.
+
+**border** 
+- That's the thickness of the border
+
+**Align**
+- The align table
+
+**Collapse**
+- Usually there are two lines in table, one to cell, other to silhouette the table it self. Default is `collapse`.
+
+**Color**
+- The color of the line the table
+
+**Text to the Table**
+- Description to the table.
+- All tables will be numbered
+
+**Footer**
+- If the last line of the table is a footer, this argument must be `True`.
+
+`Prints such a string:`
+
+![Table](readme_files/table_example.png)
+
+<br>
+<hr>
+<br>
+
+#### **Incorporate YouTube Videos**
+
+This functionality lets you embed a YouTube video player on your website and control the player using JavaScript.
+
+Using the API's JavaScript functions, you can queue videos for playback; play, pause, or stop those videos; adjust the player volume; or retrieve information about the video being played. You can also add event listeners that will execute in response to certain player events, such as a player state change.
+
+```python
+from pywpb import pywpb as wpb
+
+filename = 'test'
+
+h = wpb.header()
+b = wpb.body()
+
+b.youtube(video_id='rqz-sutSH0c', url='https://www.youtube.com/iframe_api')
+
+i = wpb.wpIO()
+i.write_file(filename,h,b)
+i.preview(filename)
+```
+
+`Prints such a string HTML5:`
+
+```html
+<iframe width="320" height="240" 
+            src="https://www.youtube.com/embed/rqz-sutSH0c" 
+            frameborder="0" allow="accelerometer; autoplay; 
+            clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+        </iframe>
+
+        <script>
+        var tag = document.createElement('script');
+
+        tag.src = "https://www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+        var player;
+        function onYouTubeIframeAPIReady() {
+            player = new YT.Player('player', {
+            height: '240',
+            width: '320',
+            videoId: 'rqz-sutSH0c',
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+            });
+        }
+
+        function onPlayerReady(event) {
+            event.target.playVideo();
+        }
+
+        var done = false;
+        function onPlayerStateChange(event) {
+            if (event.data == YT.PlayerState.PLAYING && !done) {
+            setTimeout(stopVideo, 6000);
+            done = true;
+            }
+        }
+        function stopVideo() {
+            player.stopVideo();
+        }
+        </script>
+```
+<hr>
+
+### **wpIO (web page input and output)**
+
+This method used to create an environment ways to input and output to your page created.
+
+`sintax`:
+```python
+from neublis import neublis as nb
+
+i = nb.wpIO()
+```
+
+There are no arguments to pass, but you will use the methods of that class.
+
+<hr>
+
+This module used to input and output all this that you create using Neublis. The Methods are:
+
+**Print Page**
+
+If you want to see how your page was built, this method will show you all HTML tags in your file with text output.
+
+`sintax`:
+```python
+from neublis import neublis as nb
+
+p = nb.creator('en')
+b = nb.body()
+
+print_page(self, page=p, body=b, cfg_css=False):
+```
+
+If you have been calling commands that require CSS tags, then the `css_cfg` argument should be filled with True, but we will see that later.
+
+<hr>
+
+**Load File**
+If you have a big text file to write on you page, this method is a best way to do this. After loaded, you can use the `text` to change colors of some words on the texto or put some words in bold or italic style.
+
+You must write text on you page using `w_text` method.
+
+`sintax`:
+```python
+from neublis import neublis as nb
+
+i = nb.wpIO()
+text = i.load_text('path/file.txt')
+```
+
+<hr>
+
+`enjoi!`
---
+++ source/pywpb.py
@@ -0,0 +1,325 @@
+import pandas as pd
+import numpy as np
+import os
+import webbrowser
+import seaborn as sns
+import matplotlib.pyplot as plt
+import sys
+import datetime
+import pkg_resources
+
+class header:
+    def __init__(self, charset='utf-8', 
+                 page_size=[21.0, 29.7, 2.0],
+                 margin=[0.25, 115],
+                 background='transparent',
+                 title='Neublis Page without Title'):
+        resource_package = __name__
+        resource_path = '/'.join(('templates', 'templates.dat'))
+        filepath = pkg_resources.resource_filename(resource_package, resource_path)
+        self.df_template = pd.read_csv(filepath, sep=';')
+
+        self.header = wpIO.read_template(self, file='header')
+        self.header = self.header.replace('{{charset}}', charset)
+        self.header = self.header.replace('{{dt_created}}', datetime.datetime.now().strftime('%Y/%m/%dT%H:%M:%S'))
+        self.header = self.header.replace('{{page_size1}}', str(page_size[0]))
+        self.header = self.header.replace('{{page_size2}}', str(page_size[1]))
+        self.header = self.header.replace('{{page_size3}}', str(page_size[2]))
+        self.header = self.header.replace('{{margin1}}', str(margin[0]))
+        self.header = self.header.replace('{{margin2}}', str(margin[1]))
+        self.header = self.header.replace('{{background}}', background)
+        self.header = self.header.replace('{{title}}', title)
+
+        if not os.path.exists('images'):
+            os.mkdir('images')
+        ct_img = len(os.listdir('images'))
+        if ct_img > 0:
+            files = os.listdir('images')
+            for filename in files:
+                os.remove('images/' + filename)
+
+class body:
+    def __init__(self, margin=0, line=100, 
+                 link='#000080', vlink='#800000',
+                 lang='eng'): 
+
+        resource_package = __name__
+        resource_path = '/'.join(('templates', 'templates.dat'))
+        filepath = pkg_resources.resource_filename(__name__, resource_path)
+        self.df_template = pd.read_csv(filepath, sep=';')
+
+        self.body = wpIO.read_template(self, 'body')      
+        self.body = self.body.replace('{{margin}}', str(margin))
+        self.body = self.body.replace('{{line}}', str(line))
+        self.body = self.body.replace('{{link}}', link)
+        self.body = self.body.replace('{{vlink}}', vlink)
+        self.body = self.body.replace('{{lang}}', lang)
+
+        self.cTab = 2  # indentation control tab
+        self.ct_tbl = 0 # number of tables created
+
+    # set tab control for tags around
+    def config_tab(self, nr=0):
+        self.cTab = self.cTab + nr
+        if self.cTab < 0:
+            self.cTab = 0
+        text_tab = ''
+        for i in range(self.cTab):
+            text_tab = text_tab + '\t'
+        return(text_tab)
+
+    # headings
+    def h(self, text='Headings have no text', 
+                size=5, background='white', 
+                align='left', color='black', shadow=False):
+
+        if align not in ['center', 'left', 'right', 'justify']:
+            raise ValueError('align %s not permited' % align)
+        else:
+            self.headings = wpIO.read_template(self, 'h')
+            self.headings = self.headings.replace('{{size}}', str(size))
+            self.headings = self.headings.replace('{{background}}', background)
+            self.headings = self.headings.replace('{{text}}', text)
+            self.headings = self.headings.replace('{{align}}', align)
+            self.headings = self.headings.replace('{{color}}', color)
+            
+            self.body = self.body + self.config_tab() + self.headings 
+
+    def color_text(self, text='no text provided', color='black'):
+        self.text = wpIO.read_template(self, 'color_text')
+        self.text = self.text.replace('{{text}}', text)
+        self.text = self.text.replace('{{color}}', color)
+        self.body = self.body + self.config_tab() + self.text 
+
+    def w_text(self, text='no text provided', line_feed=1):
+        self.body = self.body + self.config_tab() + text 
+        for i in range(line_feed):
+            self.body = self.body + '<BR>'
+        self.body = self.body + '\n'
+
+    def change_text_color(self, color='black',text='no text provided',
+                                text_to='provided'):
+        new_text = text.replace(text_to,'<span style="color:' + color + '">' + text_to + ' </span>')
+        return(new_text)
+
+    def bold_text(self, text='no text provided',
+                        text_to='no text to was provided'):
+        text = '\n' + self.config_tab() + text                        
+        new_text = text.replace(text_to,' <strong>' + text_to + ' </strong>')
+        return(new_text)
+
+    def italic_text(self, text='no text provided',
+                        text_to='no text to was provided'):
+        text = '\n' + self.config_tab() + text                        
+        new_text = text.replace(text_to,' <i>' + text_to + ' </i>')
+        return(new_text)
+
+    def hline(self, height=1, border=0, color='gray', background='gray'):
+        self.text = wpIO.read_template(self, 'hline')
+        self.text = self.text.replace('{{height}}', str(height))
+        self.text = self.text.replace('{{border}}', str(border))
+        self.text = self.text.replace('{{color}}', color)
+        self.text = self.text.replace('{{background}}', background)
+        self.body = self.body + self.config_tab() + self.text 
+
+
+    def w_table(self, data="none", border=1, align='left', 
+                      collapse='collapse', color='black', 
+                      alt_text='no text provided',
+                      foot=False):
+
+        if foot:
+            sum_shape = -1
+        else:
+            sum_shape = 0
+
+        if str(type(data)) != "none":
+            self.tableh = wpIO.read_template(self, 'tableh')
+            self.tableh = self.tableh.replace('{{align}}', align)
+            self.tableh = self.tableh.replace('{{collapse}}', collapse)
+            self.tableh = self.tableh.replace('{{border_size}}', str(border))
+            self.tableh = self.tableh.replace('{{border_color}}', color)
+            self.tablef = wpIO.read_template(self, 'tablef')
+            self.tableh = self.tableh.replace('{{alt_text}}', alt_text)
+            self.ct_tbl += 1
+            self.tableh = self.tableh.replace('{{ct_tbl}}', str(self.ct_tbl))
+        else:
+            raise ValueError('No dataframe provided to write table, I need one')
+
+        self.body = self.body + self.tableh
+        self.theadh = wpIO.read_template(self, 'table_theadh')
+        self.theadb = wpIO.read_template(self, 'table_theadb')
+        self.theadf = wpIO.read_template(self, 'table_theadf')
+        self.tbodyh = wpIO.read_template(self, 'table_tbodyh')
+        self.tbodyb = wpIO.read_template(self, 'table_tbodyb')
+        self.tbodyf = wpIO.read_template(self, 'table_tbodyf')
+
+        # building table head 
+        self.body = self.body + self.theadh
+        for col_names in data.columns:
+            self.field = self.theadb.replace('{{field}}', col_names)
+            self.body = self.body + self.config_tab() + self.field
+        self.body = self.body + self.theadf
+
+        # building table body
+        self.body = self.body + self.tbodyh
+        self.config_tab(2)
+        for i in range(data.shape[0] + sum_shape):
+            self.body = self.body + self.config_tab() + '<tr>\n'
+            for j in range(data.shape[1]):
+                self.field = self.tbodyb.replace('{{field}}', str(data.iloc[i][j]))
+                self.body = self.body + self.config_tab() + self.field
+            self.body = self.body + self.config_tab() + '</tr>\n'
+        self.body = self.body + self.tbodyf
+        self.config_tab(-2)
+        # building table foot
+        if foot:
+            self.tfooth = wpIO.read_template(self, 'table_tfooth')
+            self.tfootb = wpIO.read_template(self, 'table_tfootb')
+            self.tfootf = wpIO.read_template(self, 'table_tfootf')
+            i = data.shape[0] -1
+            self.body = self.body + self.tfooth
+            for j in range(data.shape[1]):
+                self.field = self.tfootb.replace('{{field}}', str(data.iloc[i][j]))
+                self.body = self.body + self.config_tab() + self.field
+            self.body = self.body + self.tfootf
+        else:
+            self.body = self.body + self.config_tab() + self.tablef
+
+
+    def w_image(self, image='none', width=350, 
+                      height=350, alt_text='no alt text was provided'):
+
+        if str(type(image)) == 'None':
+            raise ValueError('No image provided to create graph, I need one')
+
+        if not os.path.exists('images'):
+            os.mkdir('images')
+        ct_img = len(os.listdir('images')) + 1
+        plt.savefig('images/img_' + str(ct_img) + '.png')
+        
+        self.tag_image = wpIO.read_template(self, 'image')
+        self.tag_image = self.tag_image.replace('{{height}}', str(height))
+        self.tag_image = self.tag_image.replace('{{width}}', str(width))
+        self.tag_image = self.tag_image.replace('{{ct_img}}', str(ct_img))
+        self.tag_image = self.tag_image.replace('{{alt_text}}', alt_text)
+        self.body = self.body + self.tag_image
+
+    def w_footer(self, header='This page was built with Neublis', 
+                       link_email='delermando@gmail.com', name_email='Delermando', 
+                       align='center', padding=3,
+                       background_color='gray',
+                       color='black'):
+        
+        self.tag_footer = wpIO.read_template(self, 'footer')
+        self.tag_footer = self.tag_footer.replace('{{header}}', header)
+        self.tag_footer = self.tag_footer.replace('{{link_email}}', link_email)
+        self.tag_footer = self.tag_footer.replace('{{name_email}}', name_email)
+        self.tag_footer = self.tag_footer.replace('{{align}}', align)
+        self.tag_footer = self.tag_footer.replace('{{background-color}}', background_color)
+        self.tag_footer = self.tag_footer.replace('{{color}}', color)
+        self.tag_footer = self.tag_footer.replace('{{padding}}', str(padding))
+        self.body = self.body + self.tag_footer
+
+    def page_header(self, title='This title was built with Neublis', 
+                    sub_title='Author: Delermando', size=4):
+        
+        self.page_header_tag = wpIO.read_template(self, 'page_header')
+        self.page_header_tag = self.page_header_tag.replace('{{title}}', title)
+        self.page_header_tag = self.page_header_tag.replace('{{sub_title}}', sub_title)
+        self.page_header_tag = self.page_header_tag.replace('{{size}}', str(size))
+        self.body = self.body + self.page_header_tag
+
+    def show_video(self, width=320, height=240, url='none', type='mp4'):
+
+        if url == 'none':
+            raise ValueError('No valid URL, I need one')
+
+        self.video_tag = wpIO.read_template(self, 'video')
+        self.video_tag = self.video_tag.replace('{{width}}', str(width))
+        self.video_tag = self.video_tag.replace('{{height}}', str(height))
+        self.video_tag = self.video_tag.replace('{{url}}', url)
+        self.video_tag = self.video_tag.replace('{{type}}', type)
+        self.body = self.body + self.video_tag
+
+    def youtube(self, width=320, height=240, video_id='none', url='none'):
+
+        if url == 'none':
+            raise ValueError('No valid URL, I need one')
+
+        self.video_tag = wpIO.read_template(self, 'youtube')
+        self.video_tag = self.video_tag.replace('{{width}}', str(width))
+        self.video_tag = self.video_tag.replace('{{height}}', str(height))
+        self.video_tag = self.video_tag.replace('{{url}}', url)
+        self.video_tag = self.video_tag.replace('{{video_id}}', video_id)
+        self.body = self.body + self.video_tag
+
+    def w_frame(self, margin_botton=0, line_height=100, align='left',
+                width=260, border=1, solid='#000000', padding=0.15, 
+                background='#ffffff', ncols=1, gutter=19, frame_id='none',
+                text_line='none'):
+        self.frame_tag = wpIO.read_template(self, 'frame')
+        self.body = self.body + self.frame_tag
+
+class wpIO:
+
+    def __init__(self):
+        resource_package = __name__
+        resource_path = '/'.join(('templates', 'templates.dat'))
+        filepath = pkg_resources.resource_filename(resource_package, resource_path)
+        self.df_template = pd.read_csv(filepath, sep=';')
+
+    def parse(self, page='none', body='none', cfg_css='none'):
+
+        creator = str(type(page))
+        if creator == "<class 'str'>":
+            raise ValueError('Header not passed to be read')
+        text = page.header
+
+        tag_body = str(type(body))
+        if tag_body == "<class 'str'>":
+            raise ValueError('Tag Body not passed to be read')
+        
+        text = text + body.body
+
+        return(text)
+
+    def print_page(self, page='none', body='none', cfg_css='none'):
+        text = self.parse(page, body, cfg_css)
+        print(text)
+
+    def read_template(self, file):
+        resource_package = __name__
+        file_read = self.df_template[self.df_template['template'] == file]['file']
+        file_read = file_read.to_string().split(' ')[-1]
+        resource_path = '/'.join(('templates', file_read))
+        filepath = pkg_resources.resource_filename(resource_package, resource_path)
+        print('[' + filepath + ']')
+        text_file = open(filepath,"r")
+        Lines = text_file.readlines()
+        str = ""
+        for line in Lines:
+            str = str + line
+        text_file.close()        
+        return(str)
+
+    def write_file(self, filename, page='none', body='none', cfg_css='none', mode='w+'):
+        text = self.parse(page, body, cfg_css)
+        close_page = wpIO.read_template(self, 'close') 
+        text = text + close_page
+        file2write = filename + '.html'
+        file = open(file2write,mode) 
+        file.write(text)
+        file.close()
+
+    def preview(self, filename):
+        webbrowser.open('file://' + os.path.realpath(filename) + '.html').read()
+
+    def load_text(self, file):
+        text_file = open(file,'r')
+        Lines = text_file.readlines()
+        str = ""
+        for line in Lines:
+            str = str + line
+        text_file.close()        
+        return(str)
